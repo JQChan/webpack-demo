0a13cbdfdcacfafb0d62ff305062f45de214f573
JQ Chan <vikers28@icloud.com>
2017-08-06 22:35:08 +0800

    add webpack commonsChunkPlugin to split the code

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   // entry: './src/index.js',
-  entry: {
+  entry: { //文件入口
     app: './src/index.js',
     print: './src/print.js'
   },
-  output: {
+  output: { //输出文件文件夹以及文件名
     // filename: 'bundle.js',
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -52,8 +52,26 @@ module.exports = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(), // 启用 HMR
     new HtmlWebpackPlugin({
-      title: 'Multiple Output Management'
-    })
+      title: 'Code Splitting'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({ //输出公共模块
+      name: 'common', //如果设置了name，则filename中的name为此属性,必须要有，要不会报错
+      filename: '[name].js', //如果设置了filename，则以filename命名，否则以output 中的filename命名
+      minChunks: 2, //(模块必须被2个 入口chunk 共享)公共模块的数量的最小值，传入 `Infinity` 会马上生成 公共chunk，但里面没有模块。
+      // chunks: ["pageA", "pageB"],//只在pageA和pageB中选取公共模块，不会在其他js中选取,或者忽略该项设置以选择全部 chunks
+      // children: true, // (选择所有被选 chunks 的子 chunks)
+      // async: true, // (创建一个异步 公共chunk)
+    }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "vendor",
+    //   minChunks: function(module){
+    //     return module.context && module.context.indexOf("node_modules") !== -1;
+    //   }
+    // }),
+    // new webpack.optimize.CommonsChunkPlugin({ //输出webpack
+    //   name: "manifest",
+    //   minChunks: Infinity
+    // }),
   ],
   devServer: {
     hot: true, // 告诉 dev-server 我们在使用 HMR
